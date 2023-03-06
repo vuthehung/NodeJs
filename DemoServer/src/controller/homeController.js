fcc97715558ea4bcd21211a149630b3cd484c33b
vuthehung <hungvt280102@gmail.com>
2023-03-06 16:51:13 +0700

    connect db

--- DemoServer/src/controller/homeController.js
+++ DemoServer/src/controller/homeController.js
@@ -1,7 +1,23 @@
+import connection from "../configs/connectDB"
 
 let getHomePage = (req, res) => {
     //logic
-    return res.render('index.ejs')
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstname,
+                    lastName: row.lastname
+                })
+
+            });
+            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+        })
 }
 
 module.exports = {
